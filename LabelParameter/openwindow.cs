fb83a45f6321f866a691adc109beb1c3aa7d5f1c
bingbimbin <113281174+bingbimbin@users.noreply.github.com>
2025-06-24 10:15:34 +0900

    Create openwindow.cs

---
+++ LabelParameter/openwindow.cs
@@ -0,0 +1,60 @@
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using NewJeans;
+
+namespace NewJeans
+{
+
+    [Transaction(TransactionMode.Manual)]
+
+    public class LabelParameter : IExternalCommand
+    {
+        private static Window _existingWindow;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                var dummy = new OfficeOpenXml.ExcelPackage();
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("EPPlus 테스트", ex.ToString());
+            }
+
+            if (_existingWindow != null && _existingWindow.IsVisible)
+            {
+                _existingWindow.Activate();
+                return Result.Succeeded;
+            }
+
+            var handler = new FamilyParameterHandler();
+            var externalEvent = ExternalEvent.Create(handler);
+            var control = new LabelParameterResult(externalEvent, handler);
+
+            _existingWindow = new Window
+            {
+                Title = "Lable Parameter 추출",
+                Content = control,
+                Width = 450,
+                Height = 500,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb(44, 47, 56))
+            };
+
+            _existingWindow.Closed += (s, e) => _existingWindow = null;
+            _existingWindow.Show();
+
+            return Result.Succeeded;
+        }
+    }
+
+}
+
